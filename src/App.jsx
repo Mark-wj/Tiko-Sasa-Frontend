8a5ecf3caf9e8e759ff72371a875564739f98458
Mark Njenga <marknjenga25@gmail.com>
2025-02-06 17:43:49 +0300

    Create login and register components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Login from './components/Login'
+import Registration from './components/Registration'
+// import Signin from './components/Signin'
+
+
+function App() {
+
+
+  return (
+    <div>
+      <Registration />
+      {/* <Signin /> */}
+      <Login />
+    </div>
+  )
+}
+
+export default App
